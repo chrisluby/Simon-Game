3d378aeb7ba3a6c3a66e1c9599cf772b21de1a54
Christopher Luby <chrismluby@gmail.com>
2019-07-24 23:12:08 -0400

    added functionality to color blocks

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ function randomColor() {
     } else {
         let number = "blue"
         return number
-    }
-    
+    }  
 }
 
 //generate array of 100 random colors
@@ -32,4 +31,55 @@ for (i=0; i < 100; i++) {
     colors.push(randomColor())
 }
 
-//game ready-------------------------------------------------------------------------------
+//TODO: refactor
+//shows userclick and pushes the color to userAnswer array
+//* GREEN -----
+green.addEventListener("mousedown", function() { 
+    setTimeout(function(){ 
+    green.style.backgroundColor = "green"}, 150);
+
+    green.style.backgroundColor = "#00800085"; //flash color  //todo: adjust colors
+    userAnswer.push("green"); 
+    console.log(userAnswer)
+});
+
+//* RED -----
+red.addEventListener("mousedown", function() { 
+    setTimeout(function(){ 
+    red.style.backgroundColor = "red"}, 150);
+
+    red.style.backgroundColor = "#920000"; //flash color
+    userAnswer.push("red"); 
+    console.log(userAnswer)
+});
+
+//* YELLOW -----
+yellow.addEventListener("mousedown", function() { 
+    setTimeout(function(){ 
+    yellow.style.backgroundColor = "yellow"}, 150);
+
+    yellow.style.backgroundColor = "#7f8000"; //flash color  //todo: adjust colors
+    userAnswer.push("yellow"); 
+    console.log(userAnswer)
+});
+
+//* BLUE -----
+blue.addEventListener("mousedown", function() { 
+    setTimeout(function(){ 
+    blue.style.backgroundColor = "blue"}, 150);
+
+    blue.style.backgroundColor = "#000079"; //flash color  //todo: adjust colors
+    userAnswer.push("blue"); 
+    console.log(userAnswer)
+});
+
+console.log(colors)
+//* game state
+
+
+
+
+
+
+
+
