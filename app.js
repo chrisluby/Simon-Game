8fcaedc735fcbcf9e5d9680a75f2027e8b20b8d2
Christopher Luby <chrismluby@gmail.com>
2019-08-15 17:50:44 -0400

    Refactor

--- app.js
+++ app.js
@@ -3,85 +3,83 @@ let green = document.getElementById('green')
 let red = document.getElementById('red')
 let yellow = document.getElementById('yellow')
 let blue = document.getElementById('blue')
-//------------------------
+
 let userAnswer = [];
 
 function randomColor() {
-    var min = 1; 
+    var min = 1;
     var max = 1000;
     var number = Math.floor(Math.random() * (+max - +min)) + +min;
     if (number > 0 && number <= 250) {
-        let number = "green"
+        let number = "g";
         return number
     } else if (number >= 250 && number <= 500) {
-        let number = "red"
+        let number = "r";
         return number
     } else if (number >= 500 && number <= 750) {
-        let number = "yellow"
+        let number = "y";
         return number
     } else {
-        let number = "blue"
+        let number = "b";
         return number
-    }  
+    }
 }
 
 //generate array of 40 random colors
 let colors = [];
-for (i=0; i < 40; i++) {
+for (i = 0; i < 40; i++) {
     colors.push(randomColor())
 }
-
-
-colors.forEach(function(color) {
-    colorPressed()
-  });
-
-
-
+console.log(colors)
 
 //TODO: refactor
+
 //shows userclick and pushes the color to userAnswer array
 //* GREEN -----
-green.addEventListener("mousedown", function() { 
-    setTimeout(function(){ 
-    green.style.backgroundColor = "green"}, 150);
+green.addEventListener("mousedown", function () {
+    setTimeout(function () {
+        green.style.backgroundColor = "green"
+    }, 150);
 
     green.style.backgroundColor = "#00800085"; //flash color  //todo: adjust colors
-    userAnswer.push("green"); 
+    userAnswer.push("green");
     console.log(userAnswer)
 });
 
 //* RED -----
-red.addEventListener("mousedown", function() { 
-    setTimeout(function(){ 
-    red.style.backgroundColor = "red"}, 150);
+red.addEventListener("mousedown", function () {
+    setTimeout(function () {
+        red.style.backgroundColor = "red"
+    }, 150);
 
     red.style.backgroundColor = "#920000"; //flash color
-    userAnswer.push("red"); 
+    userAnswer.push("red");
     console.log(userAnswer)
 });
 
 //* YELLOW -----
-yellow.addEventListener("mousedown", function() { 
-    setTimeout(function(){ 
-    yellow.style.backgroundColor = "yellow"}, 150);
+yellow.addEventListener("mousedown", function () {
+    setTimeout(function () {
+        yellow.style.backgroundColor = "yellow"
+    }, 150);
 
     yellow.style.backgroundColor = "#7f8000"; //flash color  //todo: adjust colors
-    userAnswer.push("yellow"); 
+    userAnswer.push("yellow");
     console.log(userAnswer)
 });
 
 //* BLUE -----
-blue.addEventListener("mousedown", function() { 
-    setTimeout(function(){ 
-    blue.style.backgroundColor = "blue"}, 150);
+blue.addEventListener("mousedown", function () {
+    setTimeout(function () {
+        blue.style.backgroundColor = "blue"
+    }, 150);
 
     blue.style.backgroundColor = "#000079"; //flash color  //todo: adjust colors
-    userAnswer.push("blue"); 
+    userAnswer.push("blue");
     console.log(userAnswer)
 });
 
-console.log(colors)
+// console.log(colors)
 
 
 
@@ -91,56 +89,45 @@ console.log(colors)
 
 
 //show the buttons being pressed
-function greenPressed(){
-    setTimeout(function(){ 
-        green.style.backgroundColor = "green"}, 600);
-    
-        green.style.backgroundColor = "#00800085"; //flash color  //todo: adjust colors
-        userAnswer.push("green"); 
-        console.log(userAnswer)
+function greenPressed() {
+    setTimeout(function () {
+        green.style.backgroundColor = "green"
+    }, 600);
+
+    green.style.backgroundColor = "#00800085"; //flash color  
+    userAnswer.push("green");
 }
 
-function redPressed(){
-    setTimeout(function(){ 
-        red.style.backgroundColor = "red"}, 600);
-    
-        red.style.backgroundColor = "#920000"; //flash color
-        userAnswer.push("red"); 
-        console.log(userAnswer)
+function redPressed() {
+    setTimeout(function () {
+        red.style.backgroundColor = "red"
+    }, 600);
+
+    red.style.backgroundColor = "#920000"; //flash color
+    userAnswer.push("red");
 }
 
-function yellowPressed(){
-    setTimeout(function(){ 
-        yellow.style.backgroundColor = "yellow"}, 600);
-    
-        yellow.style.backgroundColor = "#7f8000"; //flash color  //todo: adjust colors
-        userAnswer.push("yellow"); 
-        console.log(userAnswer)
+function yellowPressed() {
+    setTimeout(function () {
+        yellow.style.backgroundColor = "yellow"
+    }, 600);
+
+    yellow.style.backgroundColor = "#7f8000"; //flash color  
+    userAnswer.push("yellow");
 }
 
-function bluePressed(){
-    setTimeout(function(){ 
-        blue.style.backgroundColor = "blue"}, 600);
-    
-        blue.style.backgroundColor = "#000079"; //flash color  //todo: adjust colors
-        userAnswer.push("blue"); 
-        console.log(userAnswer)
+function bluePressed() {
+    setTimeout(function () {
+        blue.style.backgroundColor = "blue"
+    }, 600);
+
+    blue.style.backgroundColor = "#000079"; //flash color  
+    userAnswer.push("blue");
 }
 
-bluePressed()
 
 
 
 
 
-// function showMoves() {
-//     var i = 0;
-//     var moves = setInterval(function(){
-//       playGame(colors[i]);
 
-
-//       i++;
-//       if (i >= game.currentGame.length) {
-//         clearInterval(moves);
-//       }
-//     }, 600);
